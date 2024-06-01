ee849e75e4c475fee4d9c5cd46c84cf050d114c8
Will Cooley <william.cooley@gmail.coom>
2024-05-31 22:31:37 -0400

    initial commit

---
+++ server/migrations/versions/e1736a8854c6_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: e1736a8854c6
+Revises: 
+Create Date: 2024-05-31 22:31:26.383634
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1736a8854c6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
